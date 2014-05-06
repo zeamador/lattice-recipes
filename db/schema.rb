7887994049ccffd69fc5283d7694909bce4e7102
Bessie <Bessie@d-140-142-161-134.dhcp4.washington.edu>
2014-05-06 10:31:24 -0700

    Add User Model (some more)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140506171914) do
+ActiveRecord::Schema.define(version: 20140506173014) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -21,5 +21,6 @@ ActiveRecord::Schema.define(version: 20140506171914) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["name"], name: "index_users_on_name", unique: true
 
 end

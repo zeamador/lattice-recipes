b95065535c36d5fe41b747d35809056bed6bb578
Bessie <Bessie@BessiematoMacBook-Pro.local>
2014-05-07 21:11:28 -0700

    Add tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,21 +1,21 @@
 require 'spec_helper'
 
-#describe "User pages" do
+describe "User pages" do
 
-#  subject { page }
+  subject { page }
 
-#  describe "signup page" do
-#    before { visit signup_path }
+  describe "signup page" do
+    before { visit signup_path }
 
-#    it { should have_content('Sign up') }
-#    it { should have_title(full_title('Sign up')) }
-#  end
+    it { should have_content('Sign up') }
+    it { should have_title('Sign up') }
+  end
 
-#  describe "profile page" do
-#    let(:user) { FactoryGirl.create(:user) }
-#    before { visit user_path(user) }
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
 
-#    it { should have_content(user.name) }
-#    it { should have_title(user.name) }
-#  end
-#end
+    it { should have_content(user.name) }
+    it { should have_title("User Profile") }
+  end
+end

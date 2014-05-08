3e09c7ffc40feca262d5cb6fce1f039643a14d78
Sam Courts <sam.r.courts@gmail.com>
2014-05-08 15:08:04 -0700

    Database migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140506173335) do
+ActiveRecord::Schema.define(version: 20140508014014) do
+
+  create_table "ingredients", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "kitchens", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "steps", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"

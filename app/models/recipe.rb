9aaaa340a92c9f0a532611929f0fa36610469ab4
Sam Courts <sam.r.courts@gmail.com>
2014-05-08 17:34:36 -0700

    Complete recipe class

---
+++ app/models/recipe.rb
@@ -0,0 +1,28 @@
+require "set"
+
+# Immutable single recipe
+class Recipe < ActiveRecord::Base
+  has_many :step, :ingredient
+  belongs_to :user
+
+  attr_reader :recipe_id, :title, :ingredients, :final_steps, :secret, :tags
+  alias_method :secret?, :secret
+
+  # Public: Initialize a Recipe with required features.
+  #
+  # recipe_id - Integer uniquely identifying this recipe.
+  # title - String description of recipe.
+  # ingredients - Set of Ingredient objects.
+  # final_steps - Set of Step objects in the recipe that are not prereqs 
+  #               for any other step in the recipe.
+  # secret - Boolean indicating privacy setting, true = secret, false = public.
+  # tags - Set of case-insensitive Strings.
+  def initialize(recipe_id, title, ingredients, final_steps, secret, tags)
+    @recipe_id = recipe_id
+    @title = title
+    @ingredients = ingredients
+    @final_steps = final_steps
+    @secret = secret
+    @tags = tags
+  end
+end
--- app/models/step.rb
+++ app/models/step.rb
@@ -7,17 +7,16 @@ class Step < ActiveRecord::Base
   attr_reader :description, :time, :attentiveness, :recipe_id,
               :equipment, :prereqs, :immediate_prereq, :preheat_prereq
 
-  # Public: Initialize Step with required features.
+  # Public: Initialize Step with required and optional features.
   #
   # description - Human readable String description of the step.
-  # time - Integer number of minutes the step requires.
+  # time - Positive Integer number of minutes the step requires.
   # attentiveness - Integer value where 0 = NONE, 1 = SOME, 2 = ALL.
   # recipe_id - Integer uniquely identifying the recipe this step belongs to.
   #
   # equipment - Set of EquipmentTypes constants. 
   #             Default is an empty Set.
-  # prereqs - Set of Step objects representing unchained 
-  #           prerequisite steps. 
+  # prereqs - Set of Step objects representing unchained prerequisite steps. 
   #           Default is an empty Set.
   # immediate_prereq - Step object representing this step's immediate prereq.
   #                    Default is nil.

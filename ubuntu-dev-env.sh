e504e027141daa2602a443d53cc3ea1e4faedf8c
Sam Courts <sam.r.courts@gmail.com>
2014-05-10 16:36:37 -0700

    Update script

--- ubuntu-dev-env.sh
+++ ubuntu-dev-env.sh
@@ -9,11 +9,11 @@ cd
 git clone git://github.com/sstephenson/rbenv.git .rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-exec $SHELL
+source $HOME/.bashrc
 
 git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
-exec $SHELL
+source $HOME/.bashrc
 
 rbenv install 2.1.1
 rbenv global 2.1.1

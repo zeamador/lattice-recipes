97ab8a996fd6dc132211d7042fb14f62cfa578f2
Zach Amador <zach.amador@gmail.com>
2014-05-10 23:52:06 -0700

    Fix header link_to's

--- db/schema.rb
+++ db/schema.rb
@@ -43,12 +43,6 @@ ActiveRecord::Schema.define(version: 20140511022753) do
     t.datetime "updated_at"
   end
 
-  create_table "uploads", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "title"
-  end
-
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"

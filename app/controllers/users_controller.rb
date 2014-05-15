3ec9e08769f662e472be9dc43b16bf761c283132
Bessie <Bessie@d-173-250-180-224.dhcp4.washington.edu>
2014-05-15 16:29:45 -0700

    Add Authorization. Can't change others' kitchen any more.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
-  
+  before_action :signed_in_user, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:show, :edit, :update]
+
+
   def show
     @user = User.find(params[:id])
     @kitchen = @user.kitchen
@@ -25,11 +28,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -43,5 +46,14 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    def signed_in_user
+       redirect_to root_url, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end

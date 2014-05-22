c314c6d5cdbedf98e97e2290d49e854b99e0fd40
Chris Chalmers <christopher@chalmersfamily.com>
2014-05-21 18:48:22 -0700

    Fix #95: Add getting user's kitchen settings before running algorithm

--- app/controllers/meal_schedules_controller.rb
+++ app/controllers/meal_schedules_controller.rb
@@ -2,31 +2,20 @@ class MealSchedulesController < ApplicationController
   def show
     recipe_factory = RecipeFactory.new
 
-    factory_params = Hash.new
-
     if current_user.meal.recipes.any?
       @recipe_objects = []
       current_user.meal.recipes.each do |db_recipe|
         @recipe_objects << recipe_factory.get_object_from_db_object(db_recipe)
       end
-      factory_params[:recipes] = @recipe_objects
 
-      if true#current_user.has_key?(:kitchen)
-        kitchen_object = KitchenObject.new
+      kitchen_object = KitchenObject.new
 
-        EquipmentTypes.constants.each do |constant|
-          kitchen_object[constant] = 
-            current_user.kitchen.send(constant.downcase)
-        end
-
-        factory_params[:kitchen] = kitchen_object
+      EquipmentTypes.constants.each do |constant|
+        kitchen_object[constant] = 
+          current_user.kitchen.send(constant.downcase)
       end
 
-      if true#meal_schedule_params.has_key?(:num_users)
-        factory_params[:num_users] = 1
-      end
-
-      @meal_schedule = MealScheduleFactory.combine(@recipe_objects)
+      @meal_schedule = MealScheduleFactory.combine(@recipe_objects, kitchen_object)
     end
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -15,7 +15,36 @@ class Recipe < ActiveRecord::Base
     self.tags.downcase!
   end
 
+  after_create :deduce_final_steps
+
   def self.search(query)
     where("title like ? OR tags like ?", "%#{query}%", "%#{query}%")
+  end
+
+  private
+  def deduce_final_steps
+    not_final = Set[]
+
+    # Assemble a list of step numbers of steps that are not final
+    self.steps.each do |step|
+      step.step_mappers.each do |step_mapper|        
+        not_final << step_mapper.prereq_step_number
+      end
+    end
+   
+    #DEBUG
+    puts "non-final step numbers:"
+    not_final.each do |step_number|
+      puts step_number
+    end
+
+    puts "final step numbers:"
+    # Set all steps that are prereqs to not final
+    self.steps.each do |step|
+      step.final_step = !not_final.include?(step.step_number)
+      puts step.step_number if !not_final.include?(step.step_number)
+    end
+
+    raise "a"
   end
 end

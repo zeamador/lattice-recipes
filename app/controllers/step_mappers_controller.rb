1782711c3cd2fd90c0b28cfc158a73fe7ec10d01
Chris Chalmers <christopher@chalmersfamily.com>
2014-05-21 18:48:22 -0700

    Fix #95: Add getting user's kitchen settings before running algorithm

--- app/controllers/step_mappers_controller.rb
+++ app/controllers/step_mappers_controller.rb
@@ -1,9 +1,8 @@
 class StepMappersController < ApplicationController
 
   private
-
-    def mapper_params
-      params.require(:step_mapper).permit(:immediate_prereq, :preheat_prereq,
+  def mapper_params
+    params.require(:step_mapper).permit(:immediate_prereq, :preheat_prereq,
                                           :prereq_step_number, :prereq_id)
-    end
+  end
 end

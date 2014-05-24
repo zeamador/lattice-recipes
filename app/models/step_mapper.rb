288bed698a382221ae161dcc7ccf79554adf1265
Jake Nash <jakenash23@gmail.com>
2014-05-23 18:28:16 -0700

    Change back end validation for prereq step numbers

--- app/models/step_mapper.rb
+++ app/models/step_mapper.rb
@@ -1,5 +1,5 @@
 class StepMapper < ActiveRecord::Base
   belongs_to :step
 
-  validates :prereq_step_number, numericality: {greater_than_or_equal_to: 1, less_than: :step_id}
+  validates :prereq_step_number, numericality: {greater_than_or_equal_to: 1}
 end

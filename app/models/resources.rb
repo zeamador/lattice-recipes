9a103789a430cf4b2a4595a40fc79fe2ec786ad2
Chris Chalmers <christopher@chalmersfamily.com>
2014-06-04 18:55:18 -0700

    Fix #175; Fix bug preventing Resources from utilitzing multiple ovens

--- app/models/resources.rb
+++ app/models/resources.rb
@@ -4,8 +4,13 @@ class Resources
   def initialize(kitchen, num_users)
     @free_equipment = kitchen.clone
     @free_focus = num_users * FOCUS_PER_USER
-    # The preheat step that currently owns the oven
-    @preheat_step = nil
+    # The preheat step(s) that currently own the oven(s). This is an Array whose
+    # length is the number of ovens in the passed kitchen. Nil elements indicate
+    # that no step occupies that oven.
+    @preheat_steps = []
+    kitchen[:OVEN].times do
+      @preheat_steps.push(nil)
+    end
   end
 
   def initialize_copy(other)
@@ -18,14 +23,7 @@ class Resources
   #
   # Returns true if resources were consumed, false otherwise.
   def consume(step)
-    # If this step's equipment is an oven, a preheat step owns the oven, the
-    # step is not the preheat step that owns the oven, and the step's preheat
-    # prereq is not the step that owns the oven, fail.
-    if(step.equipment == :OVEN && !@preheat_step.nil? &&
-       step != @preheat_step && @preheat_step != step.preheat_prereq)
-      return false
-    end
-
+    # Check that the equipment used by this step is free
     unless step.equipment.nil?
       if @free_equipment[step.equipment] != 0
         @free_equipment[step.equipment] -= 1
@@ -34,8 +32,20 @@ class Resources
       end
     end
 
-    unless step.preheat_prereq.nil?
-      @preheat_step = step.preheat_prereq
+    if step.equipment == :OVEN
+      if (free_index = @preheat_steps.index(nil)) != nil
+        # There is a free preheat step slot, so fill it. If step.preheat_prereq
+        # is nil, this will not modify the array
+        @preheat_steps[free_index] = step.preheat_prereq
+      else
+        # There is no free preheat step slot. We cannot consume this step
+        # unless it is one of the preheat steps in @preheat_prereqs, or its
+        # preheat prereq is one of the preheat steps in @preheat_prereqs
+        unless @preheat_steps.include?(step) ||
+               @preheat_steps.include?(step.preheat_prereq)
+          return false
+        end
+      end
     end
 
     focus_val = FocusTypes.const_get(step.focus)
@@ -54,8 +64,9 @@ class Resources
   #
   # Returns nothing.
   def release(step)
-    if step == @preheat_step
-      @preheat_step = nil
+    # If this step is a preheat step, free up the preheat step slot it occupied
+    if (index = @preheat_steps.index(step)) != nil
+      @preheat_steps[index] = nil
     end
 
     unless step.equipment.nil?
